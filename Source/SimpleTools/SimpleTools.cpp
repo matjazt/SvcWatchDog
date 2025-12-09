322b5b65eea35b28bed777a64932e98fb58b5ef4
matjazt <matjaz.terpin@gmail.com>
2025-12-09 08:23:39 +0100

    update licence text in source files

--- Source/SimpleTools/SimpleTools.cpp
+++ Source/SimpleTools/SimpleTools.cpp
@@ -3,7 +3,27 @@
  *
  * Copyright (c) 2025 Matjaž Terpin (mt.dev@gmx.com)
  *
- * Permission is hereby granted, free of charge, ... (standard MIT license).
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * ---------------------------------------------------------------------------
+ *
+ * Official repository: https://github.com/matjazt/SvcWatchDog
  */
 
 #ifdef _WIN32
@@ -190,6 +210,17 @@ string Trim(const string& str, const string& trimChars) { return TrimEx(str, tri
 string TrimLeft(const string& str, const string& trimChars) { return TrimEx(str, trimChars, ""); }
 
 string TrimRight(const string& str, const string& trimChars) { return TrimEx(str, "", trimChars); }
+
+string BytesToHexString(const vector<uint8_t>& data)
+{
+    ostringstream oss;
+    oss << std::hex << std::setfill('0');
+    for (const uint8_t byte : data)
+    {
+        oss << std::setw(2) << TOUINT(byte);
+    }
+    return oss.str();
+}
 
 string GetFileStem(const char* file)
 {
